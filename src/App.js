d368353a2a4b490c5cf7357ec24b10f6925390b6
Alexander Prikhodko <alaront.work@gmail.com>
2022-09-18 11:37:53 +0300

    clear

--- src/App.js
+++ src/App.js
@@ -3,22 +3,17 @@ import './assets/index.sass'
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
 import Main from "./pages/Main";
-import {Context} from "./Context/Context";
 import {Route, Routes} from "react-router-dom";
 import PageNotFound from "./pages/PageNotFound";
-import PageFilm from "./pages/PageFilm";
 
 function App() {
   return (
     <>
       <Header />
-        <Context>
-            <Routes>
-                <Route path="/" element={<Main />} />
-                <Route path="film/:id" element={<PageFilm />} />
-                <Route path="*" element={<PageNotFound />} />
-            </Routes>
-        </Context>
+        <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="*" element={<PageNotFound />} />
+        </Routes>
       <Footer />
     </>
   );
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,57 +1,11 @@
-import React, {useContext, useEffect} from 'react';
-import axios from "axios";
-import Preloader from "../UI/Preloader";
-import Search from "../components/Search";
-import FilmsCards from "../components/FilmsCards";
-import Pagination from "../components/Pagination";
-import {CustomFilmContext} from "../Context/Context";
+import React from 'react';
 
 const Main = () => {
-
-    const {name, setAllFilms, type, page, isSearch, setIsSearch, setTotalResults} = useContext(CustomFilmContext)
-
-
-    useEffect(() => {
-        ombdApi();
-    }, [])
-
-    useEffect(() => {
-        ombdApi();
-    }, [name, type, page])
-
-    const ombdApi = async () => {
-        setIsSearch( true)
-
-        const {data} = await axios.get(`http://www.omdbapi.com/`, {
-            params: {
-                apikey: '2787517e',
-                s: name,
-                type: type,
-                page: page
-            }
-        }).then(res => res);
-
-        await setAllFilms(data.Search ? data.Search : []);
-        await setTotalResults(Math.ceil(Number(data.totalResults) / 10));
-        await setIsSearch(false)
-    }
 
 
     return (
         <main className="container content">
-            <Search />
-
-            <div className="content__films">
-                {
-                    !isSearch
-                        ?
-                        <FilmsCards />
-                        :
-                        <Preloader />
-                }
-            </div>
-
-            <Pagination />
+            <h1>Main page</h1>
         </main>
     );
 
