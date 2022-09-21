1299b4b9fa741d8dc817a3ad75ee567560d90380
Alexander Prikhodko <alaront.work@gmail.com>
2022-09-21 20:37:19 +0300

    api logic search

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,26 +1,61 @@
 import React, {useEffect, useState} from 'react';
 import '../assets/searchForm/searchForm.sass';
+import debounce from 'lodash.debounce'
+import axios from "axios";
+import SearchFormItem from "./SearchFormItem";
 
 const SearchForm = () => {
     const [filmName, setFilmName] = useState('');
+    const [allFilms, setAllFilms] = useState([])
 
     useEffect(() => {
-        startSearch(filmName.trim());
+        searchFilm();
     }, [filmName]);
 
-    const startSearch = name => {
-        //console.log(name)
+    const startSearch = () => {
+        const name = filmName.trim();
+        console.log('startSearch')
+        if(!name) return
 
+        console.log('name', name)
+
+        axios.get(`https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword`, {
+            params: {
+                keyword: name,
+                page: 1,
+            },
+            headers: {
+                'X-API-KEY': process.env.REACT_APP_KINOPOISK_API_UNOFFICIAL_KEY,
+                'Content-Type': 'application/json',
+            }
+        }).then(res => res.data)
+            .then(res => setAllFilms(res.films))
+            .catch(res => console.error(res));
     };
+
+    const searchFilm = debounce(startSearch, 1000);
 
     return (
         <div className='search-form'>
             <h1 className='search-form__title'><span>CinemaClub</span> - библиотека фильмов и сериалов на основе API <a href='#' target='_blank'>кинопоиска</a></h1>
 
-            <form className='search-form__form' onSubmit={e => e.preventDefault()}>
-                <input type='text' placeholder='Название фильма' value={filmName} onChange={e => setFilmName(e.target.value)}/>
-                <button></button>
-            </form>
+            <div className='search-form__form-wrapper'>
+                <form className='search-form__form' onSubmit={e => e.preventDefault()}>
+                    <input type='text' placeholder='Название фильма' value={filmName} onChange={e => setFilmName(e.target.value)}/>
+                    <button></button>
+                </form>
+
+                {
+                    allFilms.length > 0 ?
+                        (
+                            <div className="search-form__result">
+                                {allFilms.map(item => <SearchFormItem key={item.filmId} pathPage={item.filmId}
+                                    title={item.nameRu ? item.nameRu : item.nameEn}
+                                    year={item.year} posterUrl={item.posterUrl}/>)
+                                }                            </div>
+                        ) : <></>
+                }
+            </div>
 
         </div>
     );
---
+++ src/components/SearchFormItem.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import search from './../assets/searchForm/search.svg'
+import {Link} from "react-router-dom";
+
+const SearchFormItem = ({pathPage, title, year, posterUrl}) => {
+    return (
+        <Link to={`/${pathPage}`} className="search-form__result-item">
+            <div className="search-form__result-item-photo">
+                <img src={posterUrl} alt={title}/>
+            </div>
+            <div className="search-form__result-item-text">
+                <h4>{title}</h4>
+                <p>{year}</p>
+            </div>
+        </Link>
+    );
+};
+
+export default SearchFormItem;
